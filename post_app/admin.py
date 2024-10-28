3ba581632e9e13c25ecbd5ed761765ced327a2e5
mania605 <kim.minsun83@gmail.com>
2024-10-28 11:26:50 +0900

    urls, views매핑

--- post_app/admin.py
+++ post_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+#관리자 페이지에 출력될 테이블 항목을 아래 필드 형태로 지정
+class PostAdmin(admin.ModelAdmin):
+  list_display = ['title', 'category', 'created', 'updated']
+
+  #관리자 페이지에 PostAdmin설정이 적용된 Post클래스 모델을 최종 등록
+  admin.site.register(Post, PostAdmin)
+
+  
---
+++ post_app/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .import views
+# posts 경로로 url접속시 views.py에 등록된 posts REST응답 함수 매팽
+urlpatterns = [
+  path('posts', views.posts, name='posts')
+]
