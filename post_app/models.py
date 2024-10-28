e7e4ef5bfec2606497bd11206e039b8acc65b1f2
mania605 <kim.minsun83@gmail.com>
2024-10-28 10:36:33 +0900

    restful api의 개념 및 drf 라이브러리가 필요한 이유 주석 정리-몇개파일더 필요

--- post_app/models.py
+++ post_app/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.utils.text import slugify
+from django.utils.crypto import get_random_string
 
 class Post(models.Model):
-  CATEGORY =(('STUDY','Study'),('DIARY','Diary'),('COMMON','Common'))
+  CATEGORY = (('STUDY','Study'), ('DIARY','Diary'), ('COMMON','Common'))
 
 title = models.CharField(max_length=100)
 body =models.TextField()
@@ -18,17 +19,14 @@ updated= models.DateTimeField(auto_now=True)
 def __str__(self):
   return self.title
 
-
 #해당 클래스가 생성하는 모델 인스턴스로 저장해주는함수
 def save(self, *args, **kwargs):
 #모델 인스턴스 생성시 slug항목이 없으면 제목값을 슬러그화해서 대신 저장
   if not self.slug:
     slug_base = slugify(self.title)
 
-
 #이미 DB상의 같은 항목의 slug게시글이 존재하면
 #기존 slug이름에 랜덤한 5글자의 고유 문자값을 적용하여 slug중복 피함
-
 if post.objects.filter(slug=slug).exists():
   slug =f'{slug_base}-{get_ramdom_string(5)}'
   #위의 조건문으로 중복회피한 새로운 슬러그를 인스턴스에 적용
@@ -36,3 +34,4 @@ if post.objects.filter(slug=slug).exists():
 
   #부모 클래스의 save함수를 super를 이용하여 위의 적용내용들을 반환해서 호출 및 저장
   super(Post, self).save(*args, **kwargs)
+
---
+++ post_app/serializers.py
@@ -0,0 +1,25 @@
+# pip install djangorestframework
+"""
+  CRUD 
+  - DB제어를 위한 4가지 개념 
+  - Create : DB에 데이터 생성 (글쓰기후 저장)
+  - Read : DB 데이터 호출 (글 읽기)
+  - Update : DB 데이터 수정 (글 수정후 저장)
+  - Delete : DB 데이터 삭제 (글 삭제)
+  
+  RESTful API (Representational State Transfer) 
+  - 제어하려고 하는 자원의 상태값 전송 방식
+  - CRUD 구현을 위해서 실제 사용해야 되는 개발 방법론을 지칭
+  - HTTP통신을 위한 메서드 방식 GET, POST, PUT, DELETE
+  
+  - GET : 데이터 조회
+  - POST : 클라이언트로부터 데이터를 전달해서 데이터 생성
+  - PUT : 기존 데이터 변경
+  - DELETE : 데이터 삭제
+  
+  Django 프레임웍에서 굳이 DRF를 써야되는 이유, DRF가 하는일
+  - 장고 프로젝트 기반의 RESTful API 제작을 돕는 라이브러리 (직렬화, 역직렬화 기능 제공)
+  - serializer (직렬화) : 장고의 모델 인스턴스를 클라이언트에 읽기 편하도록 json포맷으로 변환처리
+  - deserializer (역직렬화) : 반대로 클라이언트로부터 전달받은 json데이터를 장고 전용의 모델 인스턴스 구조로 역변환처리
+  - validation (데이터 검증) : 역직렬화시 클라이언트가 보낸 데이터를 필드별로 유효성 검사 처리
+"""
