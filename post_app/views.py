994fa2c582a4c956bacd2207c664d3a623effc19
mania605 <kim.minsun83@gmail.com>
2024-10-28 10:50:08 +0900

    views.py에 필요한 drf전용 모듈 정리

--- post_app/views.py
+++ post_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
-# Create your views here.
+from post_app.models import Post
+# PostSerializer : Post 모델을 직렬화, 역직렬화시 필요한 클래스
+from post_app.serializers import PostSerializer
+# Response : DRF에서 제공하는 응답 객체로 json형식의 응답을 쉽게 반환처리
+from rest_framework.response import Response
+# api_view : DRF에서 HTTP메서드 (GET, POST, PUT, DELETE)를 처리하도록 뷰 정의
+from rest_framework.decorators import api_view
+# status : HTTP통신 상태 코드를 제공하는 모듈, 성공이나 오류에 대한 상태 정보를 제공
+from rest_framework import status
