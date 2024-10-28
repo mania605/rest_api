670ded2723a3a3e8c51079a081074bf809fcdc8e
mania605 <kim.minsun83@gmail.com>
2024-10-28 11:55:01 +0900

    필기수정

--- post_app/models.py
+++ post_app/models.py
@@ -12,7 +12,7 @@ class Post(models.Model):
   title = models.CharField(max_length=100)
   body =models.TextField()
 #url상에서 글 상세페이지 접속시 필요한 글 고유문자값(id로도 활용가능)
-  slug=models.SlugField(unique=True, blank=True, null=True)
+  slug = models.SlugField(unique=True, blank=True, null=True)
 
 #글 저장시 카테고리 범주 지정항목(실제 관리자페이지에서 드롭다운 메뉴 형식으로 표현됨, 기본 선택항목은 COMMON으로 지정)
   category = models.CharField(max_length=10, choices= CATEGORY, default='COMMON')
@@ -24,18 +24,20 @@ def __str__(self):
   return self.title
 
 #해당 클래스가 생성하는 모델 인스턴스로 저장해주는함수
-def save(self, *args, **kwargs):a
+def save(self, *args, **kwargs):
+  slug = ''
 #모델 인스턴스 생성시 slug항목이 없으면 제목값을 슬러그화해서 대신 저장
   if not self.slug:
-  slug_base = slugify(self.title)
-
+    slug_base = slugify(self.title)
+    slug = slug_base  
 #이미 DB상의 같은 항목의 slug게시글이 존재하면
 #기존 slug이름에 랜덤한 5글자의 고유 문자값을 적용하여 slug중복 피함
-  if Post.objects.filter(slug=slug_base).exists():
-  slug_base = f'{slug_base}-{get_random_string(5)}'
+if Post.objects.filter(slug=slug).exists():
+      slug = f'{slug_base}-{get_random_string(5)}'     
+
   #위의 조건문으로 중복회피한 새로운 슬러그를 인스턴스에 적용
-  self.slug = slug_base
+    self.slug = slug    
 
   #부모 클래스의 save함수를 super를 이용하여 위의 적용내용들을 반환해서 호출 및 저장
-  super(Post, self).save(*args, **kwargs)
+    super(Post, self).save(*args, **kwargs)
 
