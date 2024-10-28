802aa640f5278df2daf8ab6d9f1c365aed3e0d97
mania605 <kim.minsun83@gmail.com>
2024-10-28 13:04:49 +0900

    중첩 조건문 오류 수정(들여쓰기로만 코드 블록 설정되니 주의) 내문제 해결안됨

--- post_app/models.py
+++ post_app/models.py
@@ -3,41 +3,33 @@ from django.utils.text import slugify
 from django.utils.crypto import get_random_string
 
 class Post(models.Model):
-    CATEGORY = (
-        ('STUDY', 'Study'), 
-        ('DIARY', 'Diary'), 
-        ('COMMON', 'Common')
-    )
+  CATEGORY = (('STUDY','Study'), ('DAIARY','Diary'), ('COMMON','Common'))
 
   title = models.CharField(max_length=100)
-  body =models.TextField()
-#url상에서 글 상세페이지 접속시 필요한 글 고유문자값(id로도 활용가능)
+  body = models.TextField()
   slug = models.SlugField(unique=True, blank=True, null=True)
+  category = models.CharField(max_length=10, choices=CATEGORY, default='COMMON')
+  created = models.DateTimeField(auto_now_add=True)
+  updated = models.DateTimeField(auto_now=True)
 
-#글 저장시 카테고리 범주 지정항목(실제 관리자페이지에서 드롭다운 메뉴 형식으로 표현됨, 기본 선택항목은 COMMON으로 지정)
-  category = models.CharField(max_length=10, choices= CATEGORY, default='COMMON')
-  created= models.DateTimeField(auto_now_add=True)
-  updated= models.DateTimeField(auto_now=True)
 
-#post 클래스에서 생성되는 인스턴스 모델의 타이틀 값을 문자열로 변환
-def __str__(self):
-  return self.title
+  def __str__(self):
+    return self.title
+    
 
-#해당 클래스가 생성하는 모델 인스턴스로 저장해주는함수
-def save(self, *args, **kwargs):
-  slug = ''
-#모델 인스턴스 생성시 slug항목이 없으면 제목값을 슬러그화해서 대신 저장
-  if not self.slug:
-    slug_base = slugify(self.title)
-    slug = slug_base  
-#이미 DB상의 같은 항목의 slug게시글이 존재하면
-#기존 slug이름에 랜덤한 5글자의 고유 문자값을 적용하여 slug중복 피함
-if Post.objects.filter(slug=slug).exists():
-      slug = f'{slug_base}-{get_random_string(5)}'     
+  def save(self, *args, **kwargs):  
+    # 중첩 조건문 형태
+    # 만약 인스턴스에 slug값이 없으면 타이틀값으로 슬러그 생성
+    if not self.slug:
+      slug_base = slugify(self.title)
+      slug = slug_base    
 
-  #위의 조건문으로 중복회피한 새로운 슬러그를 인스턴스에 적용
-    self.slug = slug    
+      # 위에서 타이틀로 슬러그가 만들진뒤 해당 슬러그에 매칭되는 모델이 있는지 확인하는 중첩 조건문
+      # 위에서 변경한 slug와 동일한 slug의 모델데이터가 있으면 다시 해당 슬러그뒤에 고유문자를 붙여처 최종 slug설정
+      if Post.objects.filter(slug=slug).exists():
+        slug = f'{slug_base}-{get_random_string(5)}'  
+        self.slug = slug 
 
-  #부모 클래스의 save함수를 super를 이용하여 위의 적용내용들을 반환해서 호출 및 저장
+    # 조건문 밖에서 실행되는 최종 모델 저장 구문
+    # 위에서 조건처리 완료된 최종 모델인스턴스 테이블에 저장
     super(Post, self).save(*args, **kwargs)
-
